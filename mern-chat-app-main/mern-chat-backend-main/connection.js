b9c578334b31f95f4f558731f186d4412b4623b9
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2024-02-14 23:40:02 +0530

    updated connection

--- mern-chat-app-main/mern-chat-backend-main/connection.js
+++ mern-chat-app-main/mern-chat-backend-main/connection.js
@@ -12,6 +12,9 @@ const password = process.env.DB_PW;
 
 
 
-mongoose.connect(`mongodb+srv://${username}:<${password}>@cluster0.4zyglzr.mongodb.net/chatAppMern?retryWrites=true&w=majority`, ()=> {
-  console.log('connected to mongodb')
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.4zyglzr.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+  console.log('Connected to MongoDB');
 })
+.catch((err) => {
+  console.error('Connection to MongoDB failed:', err.message);
+  })
